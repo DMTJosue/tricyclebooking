a8cd06fae79241980c911210d76e096b2da77fc6
Josué DA-MATHA <josuedamatha2001@gmail.com>
2023-09-27 13:31:08 +0200

    update piplenes

---
+++ deploy.py
@@ -0,0 +1,17 @@
+import requests
+username = secrets.PYTHONANYWHERE_USERNAME 
+token = secrets.PYTHONANYWHERE_API_KEY
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+elif response.status_code == 201:
+  print("Déploiement réussi!")
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
