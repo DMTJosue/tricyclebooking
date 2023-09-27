c990ed1eb9e2047126daa1f468e2a13f88de8b5e
Josué DA-MATHA <josuedamatha2001@gmail.com>
2023-09-27 21:30:20 +0200

    Update ci.jml

--- deploy.py
+++ deploy.py
@@ -1,40 +1,48 @@
-# Import des bibliothèques nécessaires
 import os
 import requests
 import zipfile
 
 # Récupération des informations d'authentification
-username = os.environ['PYTHONANYWHERE_USERNAME']  
+username = os.environ['PYTHONANYWHERE_USERNAME']   
 token = os.environ['PYTHONANYWHERE_API_KEY']
 
-# Création du fichier zip contenant les fichiers à déployer
-zipf = zipfile.ZipFile('project.zip', 'w', zipfile.ZIP_DEFLATED) 
-# Ajout des fichiers deploy.py et requirements.txt
-zipf.write('deploy.py')   
-zipf.write('requirements.txt')
-# Fermeture du fichier zip
-zipf.close()
+try:
 
-# Récupération de l'ID de l'application
-response = requests.get(
-  f"https://www.pythonanywhere.com/api/v0/user/{username}/apps/",
-  headers={'Authorization': f'Token {token}'}
-)
-# Extraction de l'ID du premier élément de la réponse (liste d'apps)  
-app_id = response.json()[0]['id']
+  # Création du fichier zip
+  zipf = zipfile.ZipFile('project.zip', 'w', zipfile.ZIP_DEFLATED)  
+  zipf.write('deploy.py')     
+  zipf.write('requirements.txt')
+  zipf.close()
 
-# Préparation des données pour le déploiement  
-files = {'directory': open('project.zip', 'rb')}
+  if os.path.exists('project.zip'):
 
-# Requête POST de déploiement
-response = requests.post(
-  f"https://www.pythonanywhere.com/api/v0/user/{username}/apps/{app_id}/deploy/",
-  headers={'Authorization': f'Token {token}'},
-  files=files
-)
+    # Récupération de l'ID de l'application
+    response = requests.get(
+      f"https://www.pythonanywhere.com/api/v0/user/{username}/apps/", 
+      headers={'Authorization': f'Token {token}'}
+    )
 
-# Vérification du statut de la réponse
-if response.status_code == 201:
-  print("Déploiement réussi!")
-else:
-  print(f"Erreur de déploiement: {response.status_code}")
+    app_id = response.json()[0]['id']
+
+    # Préparation des données de déploiement
+    files = {'directory': open('project.zip', 'rb')}
+
+    # Requête de déploiement
+    response = requests.post(
+      f"https://www.pythonanywhere.com/api/v0/user/{username}/apps/{app_id}/deploy/",  
+      headers={'Authorization': f'Token {token}'},
+      files=files
+    )
+
+    if response.status_code == 201:
+      print("Déploiement réussi!")
+    elif response.status_code == 400:  
+      print("Requête invalide")
+    else:
+      print(f"Erreur {response.status_code}: {response.text}")
+
+  else:
+    print("Erreur de création du zip")
+
+except requests.RequestException as e:
+  print("Erreur réseau: ", e)
