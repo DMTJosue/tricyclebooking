89f5e3c1ac70d7b62f72a79558ad4fac1b2fae88
Josué DA-MATHA <josuedamatha2001@gmail.com>
2023-09-27 21:22:04 +0200

    Update ci.jml

--- deploy.py
+++ deploy.py
@@ -1,16 +1,40 @@
-import os 
+# Import des bibliothèques nécessaires
+import os
 import requests
+import zipfile
 
-username = os.environ['PYTHONANYWHERE_USERNAME']
+# Récupération des informations d'authentification
+username = os.environ['PYTHONANYWHERE_USERNAME']  
 token = os.environ['PYTHONANYWHERE_API_KEY']
 
+# Création du fichier zip contenant les fichiers à déployer
+zipf = zipfile.ZipFile('project.zip', 'w', zipfile.ZIP_DEFLATED) 
+# Ajout des fichiers deploy.py et requirements.txt
+zipf.write('deploy.py')   
+zipf.write('requirements.txt')
+# Fermeture du fichier zip
+zipf.close()
+
+# Récupération de l'ID de l'application
 response = requests.get(
-  f"https://www.pythonanywhere.com/api/v0/user/{username}/cpu/",
+  f"https://www.pythonanywhere.com/api/v0/user/{username}/apps/",
   headers={'Authorization': f'Token {token}'}
 )
+# Extraction de l'ID du premier élément de la réponse (liste d'apps)  
+app_id = response.json()[0]['id']
 
-if response.status_code == 200:
-  print('CPU quota info:')
-  print(response.content)
+# Préparation des données pour le déploiement  
+files = {'directory': open('project.zip', 'rb')}
+
+# Requête POST de déploiement
+response = requests.post(
+  f"https://www.pythonanywhere.com/api/v0/user/{username}/apps/{app_id}/deploy/",
+  headers={'Authorization': f'Token {token}'},
+  files=files
+)
+
+# Vérification du statut de la réponse
+if response.status_code == 201:
+  print("Déploiement réussi!")
 else:
-  print(f'Got unexpected status code {response.status_code}')
+  print(f"Erreur de déploiement: {response.status_code}")
