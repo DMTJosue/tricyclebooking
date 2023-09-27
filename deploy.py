8b803924a920a1277a26ce5f72dfd3dbd64d3860
Josué DA-MATHA <josuedamatha2001@gmail.com>
2023-09-27 20:39:00 +0200

    Update ci.jml

--- deploy.py
+++ deploy.py
@@ -1,17 +1,16 @@
-import os
+import os 
 import requests
+
 username = os.environ['PYTHONANYWHERE_USERNAME']
 token = os.environ['PYTHONANYWHERE_API_KEY']
+
 response = requests.get(
-    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
-        username=username
-    ),
-    headers={'Authorization': 'Token {token}'.format(token=token)}
+  f"https://www.pythonanywhere.com/api/v0/user/{username}/cpu/",
+  headers={'Authorization': f'Token {token}'}
 )
+
 if response.status_code == 200:
-    print('CPU quota info:')
-    print(response.content)
-elif response.status_code == 201:
-  print("Déploiement réussi!")
+  print('CPU quota info:')
+  print(response.content)
 else:
-    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+  print(f'Got unexpected status code {response.status_code}')
